1916bb0c1b5bc9db52f3748c28dba490ff95de08
EstebanChimbo <146973177+EstebanChimbo@users.noreply.github.com>
2023-12-07 05:04:40 +0000

    Todas las paginas cargadas y funcionales en django con static

--- backend-django/Dhermapixel/urls.py
+++ backend-django/Dhermapixel/urls.py
@@ -17,8 +17,30 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import dhermapixel
+from .views import promociones
+from .views import tratamientos
+from .views import contacto
+from .views import inicioDeSesión
+from .views import dermapen
+from .views import prp
+from .views import hialuronico
+from .views import meso
+from .views import mesoterapiaCapilar
+from .views import criolipolisis
+from .views import registro
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", dhermapixel.as_view(), name="index")
+    path("", dhermapixel.as_view(), name="index"),
+    path("promociones/", promociones.as_view(), name="promociones"),
+    path("tratamientos/", tratamientos.as_view(), name="tratamientos"),
+    path("contacto/", contacto.as_view(), name="contacto"),
+    path("inicioDeSesion/", inicioDeSesión.as_view(), name="inicioDeSesión"),
+    path("dermapen/", dermapen.as_view(), name="dermapen"),
+    path("prp/", prp.as_view(), name="prp"),
+    path("hialuronico/", hialuronico.as_view(), name="hialuronico"),
+    path("meso/", meso.as_view(), name="meso"),
+    path("mesoterapiaCapilar/", mesoterapiaCapilar.as_view(), name="mesoterapiaCapilar"),
+    path("criolipolisis/", criolipolisis.as_view(), name="criolipolisis"),
+    path("registro/", registro.as_view(), name="registro")
 ]
--- backend-django/Dhermapixel/views.py
+++ backend-django/Dhermapixel/views.py
@@ -3,3 +3,35 @@ from django.views.generic import TemplateView
 class dhermapixel(TemplateView):
     template_name = "index.html"
     
+class promociones(TemplateView):
+    template_name = "pages/promociones.html"
+
+class tratamientos(TemplateView):
+    template_name = "pages/tratamientos.html"
+    
+class contacto(TemplateView):
+    template_name = "pages/contacto.html"
+
+class inicioDeSesión(TemplateView):
+    template_name = "pages/inicioDeSesión.html"
+    
+class dermapen(TemplateView):
+    template_name = "pages/dermapen.html"
+
+class prp(TemplateView):
+    template_name = "pages/prp.html"
+    
+class hialuronico(TemplateView):
+    template_name = "pages/hialuronico.html"
+
+class meso(TemplateView):
+    template_name = "pages/meso.html"
+    
+class mesoterapiaCapilar(TemplateView):
+    template_name = "pages/mesoterapia-capilar.html"
+
+class criolipolisis(TemplateView):
+    template_name = "pages/Criolipolisis.html"
+    
+class registro(TemplateView):
+    template_name = "pages/registro.html"
